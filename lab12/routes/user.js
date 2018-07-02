ae3c4ca5f753f3544ae2e7d2f380a0224ba5cd78
PC\dell <alfonso.ocsa@tecsup.edu.pe>
2018-07-01 20:54:24 -0500

    Lab12

--- lab12/routes/user.js
+++ lab12/routes/user.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
 const userController = require('../controllers/user');
 
 router.get('/',userController.find);
@@ -10,5 +9,10 @@ router.post('/',userController.create);
 router.put('/:id',userController.update);
 router.delete('/:id',userController.delete);
 
+router.post('/signup',userController.signup);
+router.post('/signin',userController.signin);
+router.post('/refresh',userController.refreshToken);
+
+router.use(userController.verifyToken);
 
 module.exports = router;
--- lab12/server.js
+++ lab12/server.js
@@ -1,38 +1,37 @@
-const express    = require('express');
-const app        = express();
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 
-
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
-app.use("/", function(req, res, next){
-	res.header('Accesss-Control-Allow-Origin', '*');
-	res.header('Accesss-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-	res.header('Accesss-Control-Allow-Headers', 'content-Type, Authorization, Content-Length, X-Requested-With');
-	next();	
+app.use("/", function(req,res,next){
+	res.header('Access-Control-Allow-Origin','*');
+	res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Headers','Content-Type, Authorization, Content-Length, X-Requested-With');
+	next();
 });
 
-app.options("/*", function(req, res, next){
+app.options("/*", function(req,res,next){
 	res.sendStatus(200);
 });
 
-
 const router = express.Router();
 
-router.get('/', function(req, res){
-	res.json({ message: 'genial! bienvenido a nuestra api'});
+router.get('/', function(req,res){
+	res.json({message: 'genial! bienvenido a nuestra api!'});
 });
 
-app.use('/api', router);
+const userRouter = require('./routes/user');
+router.use('/user',userRouter);
 
+app.use('/api', router);
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/dawa_blog');
 mongoose.Promise = global.Promise;
 
-
 app.listen(port);
-console.log('La magia sucede en este puerto' + port);
+console.log('La magia sucede en el puerto ' + port);
